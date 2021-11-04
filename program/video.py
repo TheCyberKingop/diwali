f2bb73e38b48b3d47cb65c871adba6c4e1a0e1a3
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-11-04 08:18:56 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -5,7 +5,7 @@
 import asyncio
 import re
 
-from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2
+from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2, UPDATES_CHANNEL
 from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
 from driver.veez import call_py, user
@@ -63,9 +63,10 @@ async def vplay(c: Client, m: Message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data="cbmenu"),
-                InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="cls"),
-            ]
+                InlineKeyboardButton("• Mᴇɴᴜ", callback_data="cbmenu"),
+                InlineKeyboardButton("• Cʟᴏsᴇ", callback_data="cls"),
+            ],
+            [InlineKeyboardButton("• Cʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}")],
         ]
     )
     try:
